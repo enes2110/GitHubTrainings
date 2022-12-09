85fe520e349bcc27e16c4bae50354a1df8d89886
Enes2110 <zenginenes35@gmail.com>
2022-12-09 15:35:54 +0300

    new package new commit

---
+++ src/AnotherTrial/Package2Class1.java
@@ -0,0 +1,12 @@
+package AnotherTrial;
+
+public class Package2Class1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("How to make my projests private?");
+
+
+    }
+
+}
