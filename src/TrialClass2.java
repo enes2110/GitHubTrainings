87ca43327c751b6fa3652a46260ffd8adfc1113e
Enes2110 <zenginenes35@gmail.com>
2022-12-09 15:44:42 +0300

    removed println codes

--- src/TrialClass2.java
+++ src/TrialClass2.java
@@ -2,9 +2,9 @@ public class TrialClass2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Can I commit this one? Because I have not added to version control system");
+        //Can I commit this one? Because I have not added to version control system
 
-        System.out.println("I was able to commit but the class was in  'Unversioned Files' , first one was in 'Changes' " );
+        //I was able to commit but the class was in  'Unversioned Files' , first one was in 'Changes'
 
     }
 
