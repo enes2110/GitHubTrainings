1031312be5d074e31970acbcc2e8a9b82a295dae
Enes2110 <zenginenes35@gmail.com>
2022-12-09 15:25:58 +0300

    commit of second class

---
+++ src/TrialClass2.java
@@ -0,0 +1,9 @@
+public class TrialClass2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Can I commit this one? Because I have not added to version control system");
+
+    }
+
+}
