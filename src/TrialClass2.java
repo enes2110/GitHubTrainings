b83367a4664d481703fa7e2936368d79358ff3d0
Enes2110 <zenginenes35@gmail.com>
2022-12-09 15:28:54 +0300

    added an explanation for myself

--- src/TrialClass2.java
+++ src/TrialClass2.java
@@ -4,6 +4,8 @@ public class TrialClass2 {
 
         System.out.println("Can I commit this one? Because I have not added to version control system");
 
+        System.out.println("I was able to commit but the class was in  'Unversioned Files' , first one was in 'Changes' " );
+
     }
 
 }
