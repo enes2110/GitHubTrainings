f79a77cd5392642dc4759faa2218a9c92665f8ba
Enes2110 <zenginenes35@gmail.com>
2022-12-09 15:43:39 +0300

    changes in Package2Class1

--- src/AnotherTrial/Package2Class1.java
+++ src/AnotherTrial/Package2Class1.java
@@ -4,7 +4,11 @@ public class Package2Class1 {
 
     public static void main(String[] args) {
 
-        System.out.println("How to make my projests private?");
+   //How to make my projects private?
+
+     //I guess I have to do it private in first step while sharing the project
+
+       // I will check it in the next project
 
 
     }
